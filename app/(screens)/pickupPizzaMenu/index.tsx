1b7d3961291de3ba02e570af0a7dc01997e3f8c2
Thiago <thiago.iturra@gmail.com>
2025-06-17 03:57:12 -0300

    add name input filter and two-button filter

--- app/(screens)/pickupPizzaMenu/index.tsx
+++ app/(screens)/pickupPizzaMenu/index.tsx
@@ -4,35 +4,86 @@ import { TopBar } from '../../../components/topBar/TopBar';
 import { CardDefault } from '../../../components/cardDefault/CardDefault';
 import { pickupPizzaMenuMock } from '../../../mocks/pickupPizzaMenuMock';
 import { CartPriceBar } from '../../../components/cartPriceBar/cartPriceBar';
+import SearchInput from '../../../components/searchInput/SearchInput';
+import { useState } from 'react';
 
 export default function pickupPizzaMenu() {
   const router = useRouter();
+  const [searchText, setSearchText] = useState('');
+  const [filteredPizzas, setFilteredPizzas] = useState(
+    [...pickupPizzaMenuMock].sort((a, b) => a.title.localeCompare(b.title))
+  );
+  const [activeFilter, setActiveFilter] = useState<'title' | 'price'>('title');
 
+
+  const handleSearch = (text: string) => {
+    setSearchText(text);
+  
+    const filtered = pickupPizzaMenuMock.filter((pizza) => {
+      const searchTokens = text.toLowerCase().split(' ');
+      const titleTokens = pizza.title.toLowerCase().split(' ');
+      return searchTokens.every((token) => 
+        titleTokens.some((titleToken) => titleToken.includes(token))
+      );
+    });
+  
+    setFilteredPizzas(filtered);
+  };
+
+  const handleFilterChange = (filter: 'title' | 'price') => {
+    setActiveFilter(filter);
+
+    const sortedPizzas = [...filteredPizzas].sort((a, b) => {
+      if (filter === 'title') {
+        return a.title.localeCompare(b.title);
+      } else if (filter === 'price') {
+        const priceA = Number(a.price.replace(',', '.').trim());
+        const priceB = Number(b.price.replace(',', '.').trim());
+        return priceA - priceB;
+      }
+      return 0;
+    });
+  
+    setFilteredPizzas(sortedPizzas);
+  };
   return (
     <View style={styles.container}>
         <StatusBar backgroundColor="#e03b39" barStyle="dark-content" />
         <TopBar title="Cardápio de Pizzas" goBackTo="/(screens)/pickupMenu"/>
+
+        <SearchInput placeholder='Pesquisar...' onSearch={handleSearch} onFilterChange={handleFilterChange}/>
+
         <ScrollView>
         <View style={styles.content}>
-            {pickupPizzaMenuMock.map((pizza, index) => (
-              <CardDefault key={index} style={{ width: '95%', flexDirection: "row", alignItems: 'flex-start', elevation: 3, padding: 10 }}>
-                <Image 
-                  style={styles.image}
-                  source={{
-                    uri: pizza.imageUrl?.trim()
-                      ? pizza.imageUrl
-                      : 'https://res.cloudinary.com/dml539lnv/image/upload/v1750055901/ChatGPT_Image_16_de_jun._de_2025_03_38_34_tuz8n0.png'
-                  }}
-                />
-                <View style={styles.textContent}>
-                  <View style={styles.titleRow}>
-                    <Text style={styles.title}>{pizza.title}</Text>
-                    <Text style={styles.price}>R$ {pizza.price}</Text>
-                  </View>
-                  <Text style={styles.description}>{pizza.description}</Text>
+        {filteredPizzas.map((pizza, index) => (
+            <CardDefault
+              key={index}
+              style={{
+                width: '95%',
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+                elevation: 3,
+                padding: 10,
+                marginTop: index === 0 ? 15 : 0,
+              }}
+            >
+              <Image
+                style={styles.image}
+                source={{
+                  uri: pizza.imageUrl?.trim()
+                    ? pizza.imageUrl
+                    : 'https://res.cloudinary.com/dml539lnv/image/upload/v1750055901/ChatGPT_Image_16_de_jun._de_2025_03_38_34_tuz8n0.png',
+                }}
+              />
+              <View style={styles.textContent}>
+                <View style={styles.titleRow}>
+                  <Text style={styles.title}>{pizza.title}</Text>
+                  <Text style={styles.price}>R$ {pizza.price}</Text>
                 </View>
-              </CardDefault>
-            ))}
+                <Text style={styles.description}>{pizza.description}</Text>
+              </View>
+            </CardDefault>
+          ))}
         </View>
         </ScrollView>
         <CartPriceBar/>
@@ -48,9 +99,8 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     alignItems: 'center',
-    gap: 16,
+    gap: 15,
     backgroundColor: '#fae3b3',
-    paddingTop: 25,
     paddingBottom: 85, 
   },
   image: {
